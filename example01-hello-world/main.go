996f16b616626c03fd45a6a1d245ae66e87072c5
xfenghuang <570195314@qq.com>
2022-02-24 17:19:31 +0800

    update

--- example01-hello-world/main.go
+++ example01-hello-world/main.go
@@ -6,8 +6,12 @@ func main() {
 	a := 1
 	fmt.Printf(HelloWorld("appleboy"))
 	fmt.Println("一天就學會 Go 語言")
+	fmt.Println("南京黄学习 Go 語言")
 
-	if (a >= 1) { fmt.Println("a >= 1") }
+
+	if a >= 1 {
+		fmt.Println("a >= 1")
+	}
 }
 
 func HelloWorld(user_name string) string {
