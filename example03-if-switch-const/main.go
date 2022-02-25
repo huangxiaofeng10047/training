5a9d8b196d5b7d8ffc55b0d2c54bcfe84eb0ac8a
xfenghuang <570195314@qq.com>
2022-02-25 09:49:11 +0800

    fix update

--- example03-if-switch-const/main.go
+++ example03-if-switch-const/main.go
@@ -4,17 +4,36 @@ import "fmt"
 
 const (
 	monday = iota + 1
-	tuesday
-	wednesday
-	thursday
-	friday
-	saturday
-	sunday
+	tuesday // 2
+	wednesday //3
+	thursday //4
+	friday //5
+	saturday //6 
+	sunday  //7
 )
 
+// const (
+// 	a = iota
+// 	b
+// )
+
 const (
-	a = iota
-	b
+	a = iota //0 iota=0
+	b // 1 iota=1
+	c = iota + 2 //3 iota=2
+	d // 4 iota=3
+	e = iota + 1  //iota=4
+)
+type myConst int
+
+const (
+	zero myConst = iota // iota = 0
+	one                 // iota = 1
+	three = iota + 1    // iota = 2
+	foure               // iota = 3
+	five =iota               // iota = 4
+	six      // iota = 5
+
 )
 
 func calc() (int, int) {
@@ -22,6 +41,7 @@ func calc() (int, int) {
 }
 
 func main() {
+	fmt.Println(one,three, foure, five,six)
 	fmt.Println(monday)
 	fmt.Println(tuesday)
 	fmt.Println(wednesday)
@@ -31,6 +51,10 @@ func main() {
 	fmt.Println(sunday)
 	fmt.Println(a)
 	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+
 
 	test := true
 
@@ -66,4 +90,18 @@ func main() {
 	fmt.Println(stringSlice2)
 	fmt.Println(stringSlice2[7])
 	fmt.Println(stringSlice2[8])
+
+
+	const (
+		a = iota   //0
+		b          //1
+		c          //2
+		d = "ha"   //独立值，iota += 1
+		e          //"ha"   iota += 1
+		f = 100    //iota +=1
+		g          //100  iota +=1
+		h = iota   //7,恢复计数
+		i          //8
+)
+fmt.Println(a,b,c,d,e,f,g,h,i)
 }
