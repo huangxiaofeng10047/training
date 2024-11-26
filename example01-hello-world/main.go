a62dfe58bdf569109e065fc2b15c9a49a36c103a
huangxiaofeng10047 <hxf168482@gmail.com>
2024-11-26 09:38:51 +0800

    feat: learn golang

--- example01-hello-world/main.go
+++ example01-hello-world/main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
+
+const MaxRand = 16 //声明一个具名整型常量
+// 一个函数声明
+/*
+ StatRandomNumbers 生成一些不大于MaxRand的非负随机整数
+，并且统计和返回小于和大于MaxRand/2的随机数的个数。输入参数numRands指定了要生成的随机数的总数。
+*/
+func StatRandomNumbers(numRands int) (int, int) {
+  //声明两个整型变量
+	var a, b int
+  // 一个for循环
+	for i := 0; i < numRands; i++ {
+    // 一个if语句
+		if rand.Intn(MaxRand) < MaxRand/2 {
+			a = a + 1
+		} else {
+			b++
+		}
+	}
+	return a, b  // 返回两个整型变量
+}
+const NativeWordBits = 32 << (^uint(0) >> 63) // 64 or 32
+const Is64bitOS = ^uint(0) >> 63 != 0
+const Is32bitOS = ^uint(0) >> 32 == 0
+const n  = 1 << 64
+const r  = 'a'+0x7FFFFFFF
+const x  = 3/2*0.1
+const y  = 0.1*3/2
 
 func main() {
 	a := 50
@@ -42,6 +71,38 @@ func main() {
 		sep = " "
 	}
 	fmt.Println(s)
+	// 產生亂數
+	var num = 100
+	x1, y1 := StatRandomNumbers(num)
+	print("Result：", x1, "+", y1, "=", num, "? ")
+	println(x1+yf == num)
+  	println('a' == 97)
+	println('a' == '\141')
+	println('a' == '\x61')
+	println('a' == '\u0061')
+	println('a' == '\U00000061')
+	println(0x61 == '\x61')
+	println('\u4f17' == '众')
+  println("系统多少位",NativeWordBits)
+  //声明一个字符串变量
+  var lang,website string ="Go","https://golang.org"
+  var compiled,dynamic bool = true,false 
+  var announceYear int = 2009
+  println(lang,website,compiled,dynamic,announceYear)
+  // 变量lang 和year都为新声明的变量
+  lang,year :="Go language",2007
+  //这里，只有变量createdBy是新声明的变量,
+  //变量year已经在上面声明过了，所以这里仅仅
+  //改变了它的值，或者说它被重新声明了。
+  year,createdBy := 2009,"Google Research"
+
+  lang,year="Go",2012
+
+  print(lang,"油",createdBy,"发明")
+  print("并发布于",year,"年。")
+  println()
+  println(x)
+  println(y)
 }
 
 func HelloWorld(user_name string) string {
--- example02-golang-package/main.go
+++ example02-golang-package/main.go
@@ -5,10 +5,76 @@ import (
 
 	"github.com/go-training/training/example02-golang-package/controller"
 )
+/*
+* 函数声明和调用学习
+* init 函数 可以在 main 函数执行之前执行 可以声明多个init函数
+*/
+var bob, smith = titledName("Bob"), titledName("Smith")
+func init() {
+	fmt.Println("hi,", bob)
+}
+func titledName(who string) string {
+	return "Mr. " + who
+}
+
+func init() {
+	fmt.Println("hello,", smith)
+}
+
+
+
+func SquaresOfSumAndDiff(a int64, b int64) (s int64, d int64) {
+  //x,y :=a+b,a-b
+  //s= x*x 
+  //d = y*y
+  //return // 等价于 return s,d
+  return (a+b)*(a+b),(a-b)*(a-b)
+}
+func CompareLower4bit(m,n uint32) (r bool)  {
+  r = m&0xF > n&0xf
+  return
+}
+var v = VersionString()
 
 func main() {
 	fmt.Println("一天就學會 Go 語言")
 
 	hi := controller.HelloWorld("hxf168482")
 	fmt.Println(hi)
+  
+  println(v)
+  x,y := SquaresOfSumAndDiff(3,6)
+  println(x,y)
+  b := CompareLower4bit(uint32(x),uint32(y))
+  println(b)
+
+  doNoting("GO",1)
+
+  x2,y2 := func() (s int, d int) {
+    println("匿名函数")
+    return 3,4
+}()// 一对小括号表示立即调用该匿名函数
+
+func (a,b int)  {
+  println("a*a + b*b  = ",a*a+b*b)
+
+}(x2,y2)
+
+func (x int)  {
+    //形参x 遮挡了外层声明的变量x
+    println("x*x +y*y = ",x*x+y2*y2)
+  }  (y2)
+
+  func ()  {
+    println("x*x +y*y = ",x2*x2+y2*y2)
+  }()
+}
+
+
+func VersionString() string {
+  return "v1.0"
+}
+func doNoting(string,int32){
+  //doNoth
+
 }
